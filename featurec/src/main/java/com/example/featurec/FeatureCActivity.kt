1dcf1dd687319b5a9c4de1f2315802ccf7fce34d
ThiagoCaiado <thiagocaiadodev@gmail.com.br>
2020-02-09 18:38:34 -0300

    adding module c feature

--- featurec/src/main/java/com/example/featurec/FeatureCActivity.kt
+++ featurec/src/main/java/com/example/featurec/FeatureCActivity.kt
@@ -1,4 +1,11 @@
 package com.example.featurec
 
-class FeatureCActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FeatureCActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+    }
 }
